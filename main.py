2c59a16901c2bf78b5d4bbc4a583f44b63e75215
Sebastian Henao <sebastianhenaoloaiza@gmail.com>
2025-11-09 22:09:14 -0500

    fix: update app initialization to use asyncio.run for better compatibility

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 from app import create_app
 
-app = asyncio.run(create_app())
+app = asyncio.get_event_loop().run_until_complete(create_app())
 
-if __name__ == "__main__":
-    app.run(debug=True, use_reloader=True)
+# if __name__ == "__main__":
+#     app.run(debug=True, use_reloader=True)
